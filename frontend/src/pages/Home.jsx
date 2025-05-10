e04665a35bf8dc0e2aa67086ab3d9bc529211b02
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-10 23:10:20 +0200

    Made few changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,12 +5,12 @@ const Home = () => {
   return (
     <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
 {/* {LEFT DIV} */}
-<div className='flex-[3] bg-red-400'>
+<div className='flex-[3] '>
 <Greetings/>
 </div>
 
 {/* {RIGHT DIV} */}
-<div className='flex-[2] bg-slate-400'>
+<div className='flex-[2]'>
 Hello
 </div>
     </section>
