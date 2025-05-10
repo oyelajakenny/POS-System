8c99175e0f6f2159ae2013d65011e2a75ac40186
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-10 22:44:06 +0200

    Added the greetings component to the homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Greetings from '../components/greetings'
 
 const Home = () => {
   return (
     <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
 {/* {LEFT DIV} */}
 <div className='flex-[3] bg-red-400'>
-Hello
+<Greetings/>
 </div>
 
 {/* {RIGHT DIV} */}
