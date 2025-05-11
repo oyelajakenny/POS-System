2a8a50aa631f829355bcd966dda7df38d413368d
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-11 23:26:27 +0200

    Imported the minicard component to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
-import Greetings from '../components/greetings'
+import Greetings from '../components/home/Greetings'
+import MiniCard from '../components/home/MiniCard'
+import {BsCashCoin} from "react-icons/bs"
+import {GrInProgress} from "react-icons/gr"
 
 const Home = () => {
   return (
-    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
-{/* {LEFT DIV} */}
-<div className='flex-[3] '>
-<Greetings/>
-</div>
+    <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+      {/* {LEFT DIV} */}
+      <div className="flex-[3] ">
+        <Greetings />
+        <div className='flex items-center w-full gap-3 px-8 mt-8'>
+          <MiniCard
+            title="Total Earnings"
+            icon={<BsCashCoin />}
+            number={512}
+            footerNum={1.6}
+          />
+          <MiniCard
+            title="In Progress"
+            icon={<GrInProgress />}
+            number={16}
+            footerNum={3.6}
+          />
+        </div>
+      </div>
 
-{/* {RIGHT DIV} */}
-<div className='flex-[2]'>
-Hello
-</div>
+      {/* {RIGHT DIV} */}
+      <div className="flex-[2]">Hello</div>
     </section>
-  )
+  );
 }
 
 export default Home
