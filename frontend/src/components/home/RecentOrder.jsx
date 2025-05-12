f2d5acef1fdc61c93a011ad6150c2cdf418dc779
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-12 23:35:59 +0200

    Created the Recent orders component

---
+++ frontend/src/components/home/RecentOrder.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Recentorder = () => {
+  return (
+    <div className='px-8 mt-6'>
+        <div className='w-full rounded-lg bg-[#1a1a1a] h-[450px]'>
+            <div className='flex justify-between items-center px-6 py-4'>
+<h1 className='text-[#f5f5f5] text-lg font-semibold tracking-wide'>Recent Orders</h1>
+<a href='#' className='text-[#025cca] text-sm font-semibold'>View All</a>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Recentorder
