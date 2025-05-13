15c74788f554d3e753941547d13c8000fb4b645a
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-13 12:43:44 +0200

    Imported and updated the recent order component with orderlist component

--- frontend/src/components/home/RecentOrder.jsx
+++ frontend/src/components/home/RecentOrder.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import OrderList from "./OrderList";
 
 const Recentorder = () => {
   return (
-    <div className='px-8 mt-6'>
-        <div className='w-full rounded-lg bg-[#1a1a1a] h-[450px]'>
-            <div className='flex justify-between items-center px-6 py-4'>
-<h1 className='text-[#f5f5f5] text-lg font-semibold tracking-wide'>Recent Orders</h1>
-<a href='#' className='text-[#025cca] text-sm font-semibold'>View All</a>
-            </div>
-
+    <div className="px-8 mt-6">
+      <div className="w-full rounded-lg bg-[#1a1a1a] h-[450px]">
+        <div className="flex justify-between items-center px-6 py-4">
+          <h1 className="text-[#f5f5f5] text-lg font-semibold tracking-wide">
+            Recent Orders
+          </h1>
+          <a href="#" className="text-[#025cca] text-sm font-semibold">
+            View All
+          </a>
         </div>
+        {/* {SEARCH} */}
+        <div className="flex items-center gap-4 bg-[#1f1f1f] px-5 rounded-[15px] py-4 mx-6">
+          <FaSearch className="text-[#f5f5f5]" />
+          <input
+            type="text"
+            placeholder="Search recent orders"
+            className="bg-[#1f1f1f] outline-none text-[#f5f5f5]"
+          />
+        </div>
+        {/* {order list} */}
+        <div className="mt-6 px-6 ">
+          <OrderList />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Recentorder
+export default Recentorder;
