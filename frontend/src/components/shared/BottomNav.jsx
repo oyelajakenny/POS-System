ed1c672f67f8c23ac1dc9306d18f5f07920a3c8d
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-14 12:37:14 +0200

    Implemented the Usenavigate hook in the bottom navigation component

--- frontend/src/components/shared/BottomNav.jsx
+++ frontend/src/components/shared/BottomNav.jsx
@@ -1,21 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import {  FaHome} from "react-icons/fa";
 import { CiCircleMore } from "react-icons/ci";
 import { MdTableBar, MdOutlineReorder } from "react-icons/md";
 import { BiSolidDish } from "react-icons/bi";
 
+
+
 const BottomNav = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 h-16 flex justify-around">
-      <button className=" flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px] ">
+      <button
+        onClick={() => navigate("/")}
+        className=" flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px] "
+      >
         <FaHome className="inline mr-4" size={20} />
         <p>Home</p>
       </button>
-      <button className="flex items-center justify-center text-[#f5f5f5]  w-[200px]">
+      <button
+        onClick={() => navigate("/orders")}
+        className="flex items-center justify-center text-[#f5f5f5]  w-[200px]"
+      >
         <MdOutlineReorder className="inline mr-4" size={20} />
         <p>Orders</p>
       </button>
-      <button className="flex items-center justify-center text-[#f5f5f5]  w-[200px] ">
+      <button
+        onClick={() => navigate("/table")}
+        className="flex items-center justify-center text-[#f5f5f5]  w-[200px] "
+      >
         <MdTableBar className="inline mr-4" size={20} />
         <p>Table</p>
       </button>
