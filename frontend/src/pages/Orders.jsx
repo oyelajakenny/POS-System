5b8d53e0846b4fd74e2b6c30a81334691edad294
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-14 14:52:27 +0200

    updated the order page with the Order card component

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,29 +1,70 @@
 import React from 'react'
-import OrderCard from '../components/home/OrderCard'
+import OrderCard from '../components/orders/OrderCard'
+import BackButton from '../components/shared/BackButton';
 
 const Orders = () => {
+  const [status, setStatus] = React.useState("all");
   return (
     <div className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden ">
       <div className="flex items-center justify-between px-10 py-4">
-        <h1 className="text-[#f5f5f5] text-2xl font-bold tracking-wider">
-          Orders
-        </h1>
+        <div className="flex items-center gap-4">
+          <BackButton />
+          <h1 className="text-[#f5f5f5] text-2xl font-bold tracking-wider">
+            Orders
+          </h1>
+        </div>
         <div className="flex items-center justify-around gap-4">
-          <button className="text-[#ababab] text-lg  rounded-lg px-5 py-2 hover:bg-[#383838] transition-all ease-in-out">
+          <button
+            onClick={() => setStatus("all")}
+            className={`text-[#ababab] text-lg ${
+              status === "all" && "bg-[#383838] rounded-lg px-5 py-2"
+            } rounded-lg px-5 py-2 font-semibold`}
+          >
             All
           </button>
-          <button className="text-[#ababab] text-lg bg-[#383838] rounded-lg px-5 py-2 hover:bg-[#383831] transition-all ease-in-out">
+          <button
+            onClick={() => setStatus("in progress")}
+            className={`text-[#ababab] text-lg ${
+              status === "in progress" && "bg-[#383838] rounded-lg px-5 py-2"
+            } rounded-lg px-5 py-2 font-semibold`}
+          >
             In Progress
           </button>
-          <button className="text-[#ababab] text-lg  rounded-lg px-5 py-2 hover:bg-[#383838] transition-all ease-in-out">
+          <button
+            onClick={() => setStatus("ready")}
+             className={`text-[#ababab] text-lg ${
+              status === "ready" && "bg-[#383838] rounded-lg px-5 py-2"
+            } rounded-lg px-5 py-2 font-semibold`}
+          >
             Ready
           </button>
-          <button className="text-[#ababab] text-lg  rounded-lg px-5 py-2 hover:bg-[#383838] transition-all ease-in-out">
+          <button
+            onClick={() => setStatus("completed")}
+             className={`text-[#ababab] text-lg ${
+              status === "completed" && "bg-[#383838] rounded-lg px-5 py-2"
+            } rounded-lg px-5 py-2 font-semibold`}
+          >
             Completed
           </button>
         </div>
       </div>
-      <OrderCard />
+      <div className="flex flex-wrap px-16 py-4 gap-6 overflow-y-scroll scrollbar-hide h-[calc(100vh-5rem-4rem)] justify-center">
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+      </div>
     </div>
   );
 }
